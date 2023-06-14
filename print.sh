4c597e225934297aec6f1c1d242c315f55da7503
mark <mark@smsanalytical.com>
2023-06-14 15:16:22 +0100

    added some more checks and comments

--- print.sh
+++ print.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 #
 # Linux bash script to print stl or gcode to an ender 5 using printrun
+#
 # depends on : 
 #   printcode.py in PrintRun repo located at https://github.com/kliment/Printrun
-#   slic3r for Linux located at https://github.com/slic3r/Slic3r
+#   slic3r for Linux. Repo located at https://github.com/slic3r/Slic3r
+#   mailx for Linux from mail-util / bsd-mailx via apt install command
 #
 # define custom variables
-email="" # set to your email address of leave to ignore emailing start/stop of print job
+email="" # set to your email address of leave to ignore emailing start/stop of print job. Needs mailx installed.
 slicepath="/usr/bin/slic3r"
 printcorepath="$HOME/store/3D/BatchPrinting/Printrun/printcore.py"
 filamentini="$HOME/.Slic3r/filament/MarksPLA.ini"
 printcorefolder=`dirname $printcorepath`
 linefile=$printcorefolder/line.gcode # location of the gcode to draw out a starting line of filament 
 shutdownfile=$printcorefolder/shutdown.gcode # the code to cool-down after a job
+baud=115200 #serial speed for your 3D printer
 
 #
 if [ $scan ]
@@ -59,6 +62,8 @@ if [ ! -e "$shutdownfile" ]
 then
 	echo "missing shutdown.gcode"
 	exit 0
+else	
+	echo "found shutdown.gcode"
 fi
 
 stlfile=$1
@@ -66,6 +71,8 @@ if [ ! -f "$stlfile" ]
 then
 	echo "missing file: $stlfile"
 	exit 1
+else
+	echo "found $stlfile"
 fi
 
 base="${stlfile%%.*}"
@@ -89,12 +96,11 @@ then
 	gcodefile=$1
 fi
 
-if [ $email != "" && which mailx ]
+if [ $email != "" && `which mailx` ]
 then
     echo "start of printing of $gcodefile" | mailx -s "3D Printing" $email
 fi
 
-baud=115200
 echo "printing run line code..."
 $printcorepath --baud=$baud $devfile "$linefile"
 
@@ -104,7 +110,7 @@ $printcorepath --baud=$baud $devfile "$gcodefile"
 echo "printing shutdown.gcode .."
 $printcorepath --baud=$baud $devfile "$shutdownfile"
 
-if [ $email != "" && which mailx]
+if [ $email != "" && `which mailx`]
 then
     echo "end of printing of $gcodefile" | mailx -s "3D Printing" $email
 fi
